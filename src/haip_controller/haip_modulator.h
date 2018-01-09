18ad534d9e96c51ad75b2de63a4f4be825a6a635
Gorospe <joseba.gorospe@alumni.mondragon.edu>
2018-01-09 09:27:56 +0100

    add preamble

--- src/haip_controller/haip_modulator.h
+++ src/haip_controller/haip_modulator.h
@@ -15,16 +15,16 @@
 #define HAIP_PACKET_LENGTH 				(HAIP_FRAME_MAX_LEN * HAIP_SYMBOLS_PER_BYTE + HAIP_PREAMBLE_SYMBOLS)
 #define HAIP_TX_PACKET_LENGTH 			HAIP_PACKET_LENGTH * HAIP_OVERSAMPLING_FACTOR + HAIP_SRCOS_COEFF_NUM
 
-fract32* modulate_frame(unsigned char* frame_buffer, int frame_length);
+int modulate_frame(unsigned char* frame_buffer, int frame_length, fract32* modulated_signal);
 
 void addPreamble(void);
 void mapper(unsigned char* frame_buffer, int frame_symbols);
 void upsample(int frame_symbols);
 void filter(void);
-void oscilate(void);
+void oscilate(fract32* modulated_signal);
 void addHeader(void);
 void demap_16QAM_test(int len, double phase_off, unsigned char* frame_buffer);
-void get_quadrature_inphase_test(int buffer_len);
+void get_quadrature_inphase_test(int buffer_len, fract32* modulated_signal);
 void subsample_test(int n, int t, int len);
 
 void filter_sqrcosine_test(int len);
