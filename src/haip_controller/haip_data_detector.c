451a54209b2f7d3f057f5a3b50dc3bd59dbf74e5
Ander Gonzalez Tome <ander.gonzlaezt@alumni.mondragon.edu>
2018-01-12 12:33:40 +0100

    JAJA XD LOL

--- src/haip_controller/haip_data_detector.c
+++ src/haip_controller/haip_data_detector.c
@@ -3,25 +3,36 @@
 //
 
 #include "haip_data_detector.h"
-#include "fract2float_conv.h"
-#include "fract_typedef.h"
+#include <fract2float_conv.h>
+#include <fract_typedef.h>
 
+static double avg = 0;
+static double margin = 0.15;
 
-double haip_update_avg(double old_avg, double new_value, double weight){
+double update_avg(double old_avg, double new_value, double weight);
+
+double update_avg(double old_avg, double new_value, double weight){
 	return old_avg * (1 - weight) + new_value * weight;
 }
-int haip_next_data(fract32* input, int len, double avg, double margin){
+int haip_next_data(fract32* input, int len){
 	int i = 0;
+	double sum = 0;
+	double current = 0;
 	bool data_found = false;
 	double low_lim = avg - margin;
 	double high_lim = avg + margin;
 
 	for(i = 0; i < len; i++) {
-		if(fract32_to_float(input[i]) >=  high_lim || fract32_to_float(input[i]) <= low_lim){
+		current = fr32_to_float(input[i]);
+		sum += current;
+		if(current >=  high_lim || current <= low_lim){
 			data_found = true;
 			break;
 		}
 	}
+
+	double curr_avg = sum / i;
+	avg = update_avg(avg, curr_avg, 0.5 - 0.5/(i+1));
 
 	if(data_found)
 		return i;
