1d0ba63230d8a744903fad671574ac0fd856860f
Ander Gonzalez Tome <ander.gonzlaezt@alumni.mondragon.edu>
2018-01-12 08:57:35 +0100

    PUESTO EL DATATATATATATATA DETECTOR

---
+++ src/haip_controller/haip_data_detector.h
@@ -0,0 +1,11 @@
+//
+// Created by ander on 12/01/2018.
+//
+
+#ifndef PRUEBAS_C_HAIP_DATA_DETECTOR_H
+#define PRUEBAS_C_HAIP_DATA_DETECTOR_H
+
+double haip_update_avg(double old_avg, double new_value, double weight);
+int haip_next_data(fract32* input, int len, double avg, double margin);
+
+#endif //PRUEBAS_C_HAIP_DATA_DETECTOR_H
